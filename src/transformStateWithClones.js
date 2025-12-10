b21649a1f1bd2de323269856b2d81c7cfb767fdd
Mark Chepizhny <markchep0411@gmail.com>
2025-12-10 16:04:43 +0200

    replaced for loops with swich, removed duplicates, replaced variable name

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,36 +7,36 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
   const STATE_HISTORY = [];
   const ADD_PROPERTIES = 'addProperties';
   const REMOVE_PROPERTIES = 'removeProperties';
   const CLEAR = 'clear';
+
   let currentState = structuredClone(state);
 
   for (const action of actions) {
-    let copyState = structuredClone(currentState);
+    let stateCopy = structuredClone(currentState);
 
-    if (action.type === ADD_PROPERTIES) {
-      for (const key in action.extraData) {
-        copyState[key] = action.extraData[key];
-      }
-      STATE_HISTORY.push(copyState);
+    switch (action.type) {
+      case ADD_PROPERTIES:
+        for (const key in action.extraData) {
+          stateCopy[key] = action.extraData[key];
+        }
+        break;
+
+      case REMOVE_PROPERTIES:
+        for (const key of action.keysToRemove) {
+          delete stateCopy[key];
+        }
+        break;
+
+      case CLEAR:
+        stateCopy = {};
+        break;
     }
 
-    if (action.type === REMOVE_PROPERTIES) {
-      for (const key of action.keysToRemove) {
-        delete copyState[key];
-      }
-      STATE_HISTORY.push(copyState);
-    }
-
-    if (action.type === CLEAR) {
-      copyState = {};
-      STATE_HISTORY.push(copyState);
-    }
-
-    currentState = copyState;
+    STATE_HISTORY.push(stateCopy);
+    currentState = stateCopy;
   }
 
   return STATE_HISTORY;
